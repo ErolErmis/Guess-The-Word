332697c2eecbf64a5bffba383cce983a99c3a58c
Erol <e.ermis@yahoo.com>
2024-07-04 00:13:42 +0100

    Implemented ajax call to retrieve random word and display on the page.

--- js/script.js
+++ js/script.js
@@ -1,2 +1,23 @@
-// Add some interactivity to the website
-console.log("Hello, world!");
+var word;
+$(document).ready(function(){
+    $.get("https://random-word-api.herokuapp.com/word",function(data,status){
+        console.log(data + "\n" + status);
+        word = data[0];
+        readyWord(word);
+    })
+})
+
+//The the random word from api and start to insert elements into GameLetters
+function readyWord(word) {
+    const wordBox = document.getElementById("GameLetters");
+    for(let i = 0; i<word.length; i++) {
+        const p = document.createElement("p");
+        const textNode = document.createTextNode(word.charAt(i));
+        p.appendChild(textNode);
+        wordBox.appendChild(p);
+    }
+} 
+
+function checkAnswer(answer,word){
+    return answer===word;
+}

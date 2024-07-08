0b4b6f860687043466e153d69beea8b61755ac6c
Erol <e.ermis@yahoo.com>
2024-07-08 11:55:38 +0100

    Cleaned up ajax request into $.ajax for readability and future error handling. Added html elements where the player will input their guess (lacking JavaScript at the moment to generate these dynamically).

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
 var word;
 $(document).ready(function(){
-    $.get("https://random-word-api.herokuapp.com/word",function(data,status){
-        console.log(data + "\n" + status);
-        word = data[0];
-        readyWord(word);
-    })
+    
+   $.ajax({
+    type: 'GET',
+    url: 'https://random-word-api.vercel.app/api?words=1',
+    dataType: 'json',
+    success: function(result) {
+            readyWord(result[0])
+        },
+    error: errorMessage
+   })
+
 })
 
-//The the random word from api and start to insert elements into GameLetters
+//Take the random word from api and start to insert elements into GameLetters
 function readyWord(word) {
     const wordBox = document.getElementById("GameLetters");
     for(let i = 0; i<word.length; i++) {
@@ -18,6 +24,16 @@ function readyWord(word) {
     }
 } 
 
+//TODO: Method to scramble the word to guess. Look into Fisher-Yates algorithm for this.
+function scrambleWord() {
+
+}
+
 function checkAnswer(answer,word){
     return answer===word;
+}
+
+
+function errorMessage(){
+    alert("Internal server error, please try again later.");
 }
